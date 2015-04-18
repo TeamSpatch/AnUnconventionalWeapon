02c54525a78c29fb4d2f10514b66126338bba865
Nekith <nekith@gmail.com>
2015-04-18 18:58:23 +0200

    Alter mower speed according to grass level

--- Assets/Scripts/MowerMovement.cs
+++ Assets/Scripts/MowerMovement.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class MowerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float speedLevelMod;
     public float turningRate;
 
     Vector3 destination;
     Vector3 direction;
+    float speed;
     Gardener gardener;
 
     void Start()
@@ -24,16 +26,30 @@ public class MowerMovement : MonoBehaviour
             float z = transform.position.z + gardener.tileSize * gardener.voxelSize * Input.GetAxisRaw("Vertical");
             if (Input.GetAxisRaw("Horizontal") != 0 && x >= 0 && x <= gardener.voxelSize * gardener.tileSize * gardener.gardenSize.x) {
                 destination.x = x;
-                direction = destination - transform.position;
+                UpdateMovement();
             } else if (Input.GetAxisRaw("Vertical") != 0 && z >= 0 && z <= gardener.voxelSize * gardener.tileSize * gardener.gardenSize.y) {
                 destination.z = z;
-                direction = destination - transform.position;
+                UpdateMovement();
             }
         } else {
-            transform.position = Vector3.MoveTowards(transform.position, destination, Time.fixedDeltaTime * moveSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.fixedDeltaTime * speed);
         }
         float y = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(0, y, 0);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turningRate * Time.fixedDeltaTime);
+    }
+
+    void UpdateMovement()
+    {
+        direction = destination - transform.position;
+        speed = moveSpeed;
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Grass");
+        foreach (GameObject grass in tiles) {
+            if (destination.x >= grass.transform.position.x && destination.x < grass.transform.position.x + gardener.voxelSize) {
+                if (destination.z >= grass.transform.position.z && destination.z < grass.transform.position.z + gardener.voxelSize) {
+                    speed = speed - speedLevelMod * grass.GetComponent<Grass>().Level;
+                }
+            }
+        }
     }
 }
