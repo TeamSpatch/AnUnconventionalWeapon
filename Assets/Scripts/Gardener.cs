b2ad2b9b9676096d5cb1c9e098713c3e58964d69
Nekith <nekith@gmail.com>
2015-04-18 15:36:52 +0200

    Handle checkerboard

---
+++ Assets/Scripts/Gardener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gardener : MonoBehaviour
+{
+    public Vector2 size;
+    public float unitSize;
+
+    void Start()
+    {
+        for (int x = 0; x < unitSize * size.x; x++) {
+            for (int y = 0; y < unitSize * size.y; y++) {
+                GameObject voxel = Instantiate(Resources.Load("voxel"), new Vector3(x * 1f / unitSize, 0, y * 1f / unitSize), Quaternion.identity) as GameObject;
+                voxel.transform.localScale = new Vector3(1f / unitSize, 1f / 18f, 1f / unitSize);
+                voxel.transform.parent = transform;
+            }
+        }
+    }
+}
