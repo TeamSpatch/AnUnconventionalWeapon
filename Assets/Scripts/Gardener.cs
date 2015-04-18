58aab142c574af1bc48ef4a7eae8ee2018aeceb7
Nekith <nekith@gmail.com>
2015-04-18 18:08:18 +0200

    Random garden state

--- Assets/Scripts/Gardener.cs
+++ Assets/Scripts/Gardener.cs
@@ -9,17 +9,23 @@ public class Gardener : MonoBehaviour
 
     void Start()
     {
-        int level = Random.Range(0, 5);
-        for (float x = 0; x < tileSize * voxelSize * gardenSize.x; x += voxelSize) {
-            for (float y = 0; y < tileSize * voxelSize * gardenSize.y; y += voxelSize) {
-                if (x % (tileSize * voxelSize) == 0) {
-                    level = Random.Range(0, 5);
+        for (float x = 0; x < gardenSize.x; x++) {
+            for (float y = 0; y < gardenSize.y; y++) {
+                int level = Random.Range(2, 5);
+                for (float i = 0; i < tileSize; i++) {
+                    for (float j = 0; j < tileSize; j++) {
+                        SpawnVoxel(x * tileSize * voxelSize + i * voxelSize, y * tileSize * voxelSize + j * voxelSize, level);
+                    }
                 }
-                GameObject voxel = Instantiate(Resources.Load("Grass"), new Vector3(x, 0, y), Quaternion.identity) as GameObject;
-                voxel.transform.localScale = new Vector3(voxelSize, 0, voxelSize);
-                voxel.GetComponent<Grass>().Level = level;
-                voxel.transform.parent = transform;
             }
         }
+    }
+
+    void SpawnVoxel(float x, float y, int level)
+    {
+        GameObject voxel = Instantiate(Resources.Load("Grass"), new Vector3(x, 0, y), Quaternion.identity) as GameObject;
+        voxel.transform.localScale = new Vector3(voxelSize, 0, voxelSize);
+        voxel.GetComponent<Grass>().Level = level;
+        voxel.transform.parent = transform;
     }
 }
