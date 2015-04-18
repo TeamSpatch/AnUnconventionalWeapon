a66f6dcebb0052cbe99e1ab7336cfd948a395f28
Nekith <nekith@gmail.com>
2015-04-18 18:45:16 +0200

    Limit camera in map bounds

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,14 +6,27 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     Vector3 offset;
+    Gardener gardener;
 
     void Start()
     {
+        gardener = GameObject.Find("Garden").GetComponent<Gardener>();
         offset = transform.position - target.position;
     }
 
     void Update()
     {
-        transform.position = target.position + offset;
+        Vector3 pos = target.position + offset;
+        if (pos.x < 5 * gardener.voxelSize * gardener.tileSize) {
+            pos.x = 5 * gardener.voxelSize * gardener.tileSize;
+        } else if (pos.x > (gardener.gardenSize.x - 5) * gardener.voxelSize * gardener.tileSize) {
+            pos.x = (gardener.gardenSize.x - 5) * gardener.voxelSize * gardener.tileSize;
+        }
+        if (pos.z < -2 * gardener.voxelSize * gardener.tileSize) {
+            pos.z = -2 * gardener.voxelSize * gardener.tileSize;
+        } else if (pos.z > (gardener.gardenSize.y - 11) * gardener.voxelSize * gardener.tileSize) {
+            pos.z = (gardener.gardenSize.y - 11) * gardener.voxelSize * gardener.tileSize;
+        }
+        transform.position = pos;
     }
 }
