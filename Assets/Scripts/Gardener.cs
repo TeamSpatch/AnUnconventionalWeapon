974f54ce4b29d3190122c6284f85b76295aede90
Nekith <nekith@gmail.com>
2015-04-18 17:54:02 +0200

    Grass levels

--- Assets/Scripts/Gardener.cs
+++ Assets/Scripts/Gardener.cs
@@ -9,10 +9,15 @@ public class Gardener : MonoBehaviour
 
     void Start()
     {
-        for (int x = 0; x < tileSize * voxelSize * gardenSize.x; x++) {
-            for (int y = 0; y < tileSize * voxelSize * gardenSize.y; y++) {
-                GameObject voxel = Instantiate(Resources.Load("Voxel"), new Vector3(x * voxelSize, 0, y * voxelSize), Quaternion.identity) as GameObject;
-                voxel.transform.localScale = new Vector3(voxelSize, 1f / 18f, voxelSize);
+        int level = Random.Range(0, 5);
+        for (float x = 0; x < tileSize * voxelSize * gardenSize.x; x += voxelSize) {
+            for (float y = 0; y < tileSize * voxelSize * gardenSize.y; y += voxelSize) {
+                if (x % (tileSize * voxelSize) == 0) {
+                    level = Random.Range(0, 5);
+                }
+                GameObject voxel = Instantiate(Resources.Load("Grass"), new Vector3(x, 0, y), Quaternion.identity) as GameObject;
+                voxel.transform.localScale = new Vector3(voxelSize, 0, voxelSize);
+                voxel.GetComponent<Grass>().Level = level;
                 voxel.transform.parent = transform;
             }
         }
---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grass : MonoBehaviour
+{
+    public float unit;
+    public Material[] materials;
+
+    public int Level
+    {
+        get
+        {
+            return level;
+        }
+        set
+        {
+            level = value % 5;
+            MeshRenderer renderer = transform.FindChild("Graphics").GetComponent<MeshRenderer>();
+            renderer.material = materials[level];
+            if (level == 0) {
+                transform.localScale = new Vector3(transform.localScale.x, unit, transform.localScale.z);
+            } else if (level == 1) {
+                transform.localScale = new Vector3(transform.localScale.x, unit * 2, transform.localScale.z);
+            } else if (level == 2) {
+                transform.localScale = new Vector3(transform.localScale.x, unit * 3, transform.localScale.z);
+            } else if (level == 3) {
+                transform.localScale = new Vector3(transform.localScale.x, unit * 4, transform.localScale.z);
+            } else if (level == 4) {
+                transform.localScale = new Vector3(transform.localScale.x, unit * 5, transform.localScale.z);
+            }
+        }
+    }
+
+    int level = 0;
+}
