bb466e40556f531a593ec991cb28af7bb50a1679
Nekith <nekith@gmail.com>
2015-04-19 19:31:13 +0200

    Title and instructions

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour
+{
+    public void OnInstructions()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void OnStart()
+    {
+        Application.LoadLevel(2);
+    }
+}
